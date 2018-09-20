bb027ec41f3e45a79173c5c5e40aa91e9fe408bd
ccampos <ccampos@exact.com.pe>
2018-09-20 18:41:56 -0500

    3 INTENTOS BLOQUEO

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
-import {FormGroup, FormControl, Validators} from '@angular/forms';
-import {NgForm}  from '@angular/forms';
-import {Router} from '@angular/router';
-import {MatDialog} from '@angular/material';
-import {AuthService} from '../core/auth.service';
-import {TokenStorage} from '../core/token.storage';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { AuthService } from '../core/auth.service';
+import { TokenStorage } from '../core/token.storage';
 
 
- //const TOKEN_KEY = 'AuthTokenChris';
+//const TOKEN_KEY = 'AuthTokenChris';
 
 @Component({
   selector: 'app-login',
@@ -17,43 +17,68 @@ import {TokenStorage} from '../core/token.storage';
 })
 export class LoginComponent {
 
-  forma:FormGroup;
-  count: number = 0;
-  buttonDisabled: boolean = false;
-
-  constructor(private router: Router, 
-              public dialog: MatDialog, 
-              private authService: AuthService, 
-              private token: TokenStorage) {
-                
-
-                this.forma = new FormGroup({
-                  'usuario' : new FormControl('',     [Validators.required,Validators.minLength(4)]), 
-                  'contraseña' : new FormControl('',  Validators.required)
-                })
-
-              }
+  forma: FormGroup;
+  error = "";
+  count = 0;
+  segundos = 3;
+  // buttonDisabled: boolean = false;
 
 
-  login(){
-      this.authService.attemptAuth(this.forma.value.usuario, this.forma.value.contraseña).subscribe(
-           data => {
-              this.token.saveToken(data.token);
-              console.log(this.forma.value);
-              console.log("El usuario ingresó y se creó su token");
-              this.router.navigate(['./hola'])
-                  }
-          );
-  }
 
-  contar(contando : number){
-      if(contando%3===0 && contando!=0){
-        return this.buttonDisabled=true;
-      }else
-        return this.buttonDisabled=false;
+  constructor(private router: Router,
+    public dialog: MatDialog,
+    private authService: AuthService,
+    private token: TokenStorage) {
+
+
+    this.forma = new FormGroup({
+      'usuario': new FormControl('', [Validators.required, Validators.minLength(4)]),
+      'contraseña': new FormControl('', Validators.required)
+    })
+    console.log(this.forma);
   }
 
 
+  login() {
+    this.authService.attemptAuth(this.forma.value.usuario, this.forma.value.contraseña).subscribe(
+      data => {
+        if (data) {
+          console.log(this.forma);
+          this.token.saveToken(data.token);
+          this.router.navigate(['./hola'])
+        }
+        else {
+          this.error = "Usuario o Password incorrecto";
+        }
+      },
+      error => {
+        this.error = "Usuario o Password incorrecto 2";
+      }
+    );
 
-  
+    this.count ++;
+    if (this.count === 3) {
+      this.mostrarSegundos();
+      setTimeout(() => {
+        this.count=0;
+      } , 3000);
+    }
+    
+  }
+
+  mostrarSegundos(){
+    setTimeout(() =>{
+      this.segundos--;
+      if (this.segundos > 0) {
+        this.mostrarSegundos();
+      }      
+    }, 1000)
+  }
+
+ 
+
+      // setTimeout(this.desactivarboton, 3000);
+
+
+
 }
