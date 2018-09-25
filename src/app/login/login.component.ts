e2407dfdc272ea47472f0006c3fc8df8b7f84e96
RonaldSantosR <rsantos@exact.com.pe>
2018-09-25 11:38:36 -0500

    commit 3er Intento Valid

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,7 +10,7 @@ import { TokenStorage } from '../core/token.storage';
 import swal from 'sweetalert2'
 
 
-//const TOKEN_KEY = 'AuthTokenChris';
+
 
 @Component({
   selector: 'app-login',
@@ -22,12 +22,7 @@ export class LoginComponent {
   forma: FormGroup;
   error = "";
   count = 0;
-  entro = 0;
   segundos = 5;
-  
-  // buttonDisabled: boolean = false;
-
-
 
   constructor(private router: Router,
     public dialog: MatDialog,
@@ -44,18 +39,82 @@ export class LoginComponent {
 
 
   login() {
-    this.count++;
+
     console.log(this.count);
 
+    if (this.forma.get("usuario").value === null || this.forma.get("usuario").value.length === 0 ||
+      this.forma.get("contraseña").value === null || this.forma.get("contraseña").value.length === 0) {
+      console.log("Ingrese todos los datos");
+      swal({
+        title: 'ERROR',
+        position: 'top',
+        text: 'Por favor, ingrese todos los datos',
+        type: 'error',
+        allowOutsideClick: false
+      })
+      this.count++;
+
+    } else {
+
+      this.authService.attemptAuth(this.forma.value.usuario, this.forma.value.contraseña).subscribe(
+        data => {
+          if (data) {
+            console.log(this.forma);
+            this.token.saveToken(data.token);
+            //window.location.href = data.link;
+            this.router.navigate(['./hola'])
+          }
+        },
+        error => {
+          switch (error.status) {
+            case 403:
+              swal({
+                title: 'ERROR',
+                position: 'top',
+                text: 'El Usuario se encuentra inactivo',
+                type: 'warning',
+                allowOutsideClick: false
+              })
+              this.count++;
+              break;
+            case 401:
+              swal({
+                title: 'ERROR',
+                position: 'top',
+                text: 'El Usuario y/o Password son incorrectos',
+                type: 'error',
+                allowOutsideClick: false
+              })
+              this.count++;
+              break;
+            default:
+              swal({
+                title: 'ERROR',
+                position: 'top',
+                text: 'ERROR INTERNO DE SERVIDOR',
+                type: 'error',
+                allowOutsideClick: false
+              })
+              break;
+          }
+        this.bloquear()
+        }
+      );
+    }
+    this.bloquear();
+  }
+
+  bloquear(){
     if (this.count === 3) {
-      swal({title:'ADVERTENCIA', 
-            position:'top',
-            text:'Ha intentado ingresar mas veces de lo permitido, espere un momento' , 
-            type:'info', 
-            allowOutsideClick: false,//no cerrar
-            timer: 5000,
-            showConfirmButton: false //quitar botón
-          })
+      swal({
+        title: 'ADVERTENCIA',
+        position: 'top',
+        text: 'Ha intentado ingresar mas veces de lo permitido, espere un momento',
+        type: 'info',
+        allowOutsideClick: false,//no cerrar
+        timer: 5000,
+        showConfirmButton: false //quitar botón
+      })
       this.mostrarSegundos();
       setTimeout(() => {
         this.count = 0;
@@ -63,70 +122,8 @@ export class LoginComponent {
       this.segundos = 5;
       return;
     }
-  
-
-
-    if (this.forma.get("usuario").value === null || this.forma.get("usuario").value.length === 0 ||
-      this.forma.get("contraseña").value === null || this.forma.get("contraseña").value.length === 0) {
-      console.log("Ingrese todos los datos");
-      swal({title:'ERROR',
-           position:'top', 
-           text:'Por favor, ingrese todos los datos', 
-           type:'error',
-           allowOutsideClick: false})
-      return;
-    }
-
-
-
-    this.authService.attemptAuth(this.forma.value.usuario, this.forma.value.contraseña).subscribe(
-      data => {
-        if (data) {
-          console.log(this.forma);
-          this.token.saveToken(data.token);
-          this.entro = 1;
-          window.location.href = data.link;
-        } 
-      },
-      error => {
-        switch (error.status) {
-          case 403:
-           swal({title:'ERROR', 
-            position:'top',
-            text:'El Usuario se encuentra inactivo', 
-            type:'warning',
-            allowOutsideClick: false})
-            break;
-          case 401:
-            swal({title:'ERROR', 
-            position:'top',
-            text:'El Usuario y/o Password son incorrectos', 
-            type:'error',
-            allowOutsideClick: false})
-            break;
-          default:
-            swal({title:'ERROR', 
-            position:'top',
-            text:'ERROR INTERNO DE SERVIDOR', 
-            type:'error',
-            allowOutsideClick: false})
-            break;
-        }
-      }
-    );
-
-
-
-
-
-
-
 
   }
-
-
-
-
 
   mostrarSegundos() {
     setTimeout(() => {
@@ -136,5 +133,8 @@ export class LoginComponent {
       }
     }, 1000)
   }
+
+
+
 
 }
